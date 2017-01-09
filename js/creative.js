3cfc8b2b4c2d82628988367842d24706569ba105
Jennifer Spriggs <stellaadams@Stellas-iMac-160.local>
2017-01-08 21:16:36 -0800

    using vh instead of javascript to make divs full window height

--- js/creative.js
+++ js/creative.js
@@ -7,39 +7,21 @@
         console.log($($anchor.attr('href')).offset());
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top)
-        }, 1250, 'easeInOutExpo');
+        }, 1250, 'easeInOutExpo'); 
         event.preventDefault();
     });
 
     // Scale div to be size of viewport
-    function thirty_pc() {
-        var height = $(window).height();
-        var thirtypc = (100 * height) / 100 ;
-        thirtypc = parseInt(thirtypc) + 'px';
-        $(".full, .header-content").css('height',thirtypc);
-    }
+    // function thirty_pc() {
+    //     var height = $(window).height();
+    //     var thirtypc = (100 * height) / 100 ;
+    //     thirtypc = parseInt(thirtypc) + 'px';
+    //     $(".full, .header-content").css('height',thirtypc);
+    // }
 
-    $(document).ready(function() {
-        thirty_pc();
-        $(window).bind('resize', thirty_pc);
-    });
+    // $(document).ready(function() {
+    //     thirty_pc();
+    //     $(window).bind('resize', thirty_pc);
+    // });
 
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
-
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
 })(jQuery); // End of use strict
