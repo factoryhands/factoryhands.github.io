a1613958a7e5c9bfdfce0cb043055e34b26c513e
factoryhands <jenniferlspriggs@gmail.com>
2017-06-15 01:06:55 -0700

    adding scrolling for more to bottom

--- js/theme.js
+++ js/theme.js
@@ -2,3 +2,12 @@ $(window).on('load', function() {
     $('#preloader').fadeOut( 1000 );
     $('body').css({'overflow':'visible'}); 
 })
+
+	$(".scroll,.scroll-btn").click(function(e) {
+		e.preventDefault();
+
+		$.scrollify.next();
+
+
+
+	});
