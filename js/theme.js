69ab4999defd623f970887f01168c0f5d8095327
factoryhands <jenniferlspriggs@gmail.com>
2017-06-22 20:09:47 -0700

    adding changes before i fix this thing

--- js/theme.js
+++ js/theme.js
@@ -1,23 +1,48 @@
 $(window).on('load', function() {
     $('#preloader').fadeOut( 1000 );
-    $('body').css({'overflow':'visible'}); 
-})
-
-
-$('section').mouseenter(function() {
-    $('nav a[href="#'+$(this).attr('id')+'"]').addClass('active').siblings('nav a').removeClass('active');
+    $('body').css({'overflow':'visible'});
 });
 
-$('nav a').click(function() {
-    $(this).addClass('active').siblings('nav a').removeClass('active');
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top + 'px' }, 800, 'linear');    
+    $(".menu-link").click(function(e) {
+        e.preventDefault();
+    
+        $(".menu-overlay").toggleClass("open");
+        $(".menu").toggleClass("open");
+    });
+
+    $('section').mouseenter(function() {
+        $('nav a[href="#'+$(this).attr('id')+'"]').addClass('active').siblings('nav a').removeClass('active');
+    });
+
+    $('nav a').click(function() {
+        $(this).addClass('active').siblings('nav a').removeClass('active');
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top + 'px' }, 800, 'linear');    
+    });
+
+$(function() {
+    $.scrollify({
+        section : ".portfolio-section",
+        interstitialSection : "",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset : 0,
+        scrollbars: true,
+        standardScrollElements: "",
+        setHeights: true,
+        overflowScroll: true,
+        updateHash: true,
+        touchScroll:true,
+        before:function() {},
+        after:function() {},
+        afterResize:function() {},
+        afterRender:function() {}
+    }); 
 });
 
 
 
-$(".scroll,.scroll-btn").click(function(e) {
-    e.preventDefault();
 
-    $.scrollify.next();
-});
+
+
+
 
