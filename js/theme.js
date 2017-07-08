b16fd92e61a39d4a73127b25ef231e9b2271d56a
factoryhands <jenniferlspriggs@gmail.com>
2017-07-07 17:50:34 -0700

    adding stuff for readability

--- js/theme.js
+++ js/theme.js
@@ -3,54 +3,46 @@ $(window).on('load', function() {
     $('body').css({'overflow':'visible'});
 });
 
-    $(".menu-link").click(function(e) {
-        e.preventDefault();
+$(".menu-link").click(function(e) {
+  e.preventDefault();
     
-        $(".menu-overlay").toggleClass("open");
-        $(".menu").toggleClass("open");
-        $("body").toggleClass("no-scroll");
+  $(".menu-overlay").toggleClass("open");
+  $(".menu").toggleClass("open");
+  $("body").toggleClass("no-scroll");
+});
+
+
+
+$.scrollify({
+  section:".portfolio-section",
+  scrollbars:false,
+  offset: -50,
+  before:function(i,panels) {
+    var ref = panels[i].attr("data-section-name");
+    $(".pagination .active").removeClass("active");
+    $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+  },
+  afterRender:function() {
+    var pagination = "<ul class=\"pagination\">";
+    var activeClass = "";
+    $(".panel").each(function(i) {
+      activeClass = "";
+      if(i===0) {
+        activeClass = "active";
+      }
+      pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
     });
-
-
-
-  $.scrollify({
-    section:".portfolio-section",
-    scrollbars:false,
-    before:function(i,panels) {
-
-      var ref = panels[i].attr("data-section-name");
-
-      $(".pagination .active").removeClass("active");
-
-      $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
-    },
-    afterRender:function() {
-      var pagination = "<ul class=\"pagination\">";
-      var activeClass = "";
-      $(".panel").each(function(i) {
-        activeClass = "";
-        if(i===0) {
-          activeClass = "active";
-        }
-        pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
-      });
-
-      pagination += "</ul>";
-
-      $(".home").append(pagination);
-      /*
-
-      Tip: The two click events below are the same:
-
-      $(".pagination a").on("click",function() {
-        $.scrollify.move($(this).attr("href"));
-      });
-
-      */
-      $(".pagination a").on("click",$.scrollify.move);
-    },
-
-  });
+    pagination += "</ul>";
+    $(".home").append(pagination);
+    /*
+    Tip: The two click events below are the same:
+    $(".pagination a").on("click",function() {
+      $.scrollify.move($(this).attr("href"));
+    });
+    */
+    $(".pagination a").on("click",$.scrollify.move);
+  },
+});
 
 
 
