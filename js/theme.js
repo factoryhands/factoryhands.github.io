be5cecf8a6a0879b0628d0ba544c44ea8982eaf7
factoryhands <jenniferlspriggs@gmail.com>
2018-05-03 23:42:54 -0700

    adding animation for svg

--- js/theme.js
+++ js/theme.js
@@ -60,6 +60,15 @@ if(!(/Android|webOS|BlackBerry|iPhone|iPod|iPad|Opera Mini|IEMobile/i.test(navig
   });
 }; 
 
+var lineDrawing = anime({
+  targets: '#lineDrawing .lines path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'linear',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: false
+});
 
 
 
